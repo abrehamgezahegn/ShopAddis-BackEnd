440a9ab4b6b599052fb054b87b6fea29f3ad78a9
Abrehamgit <gezahegnabereham@gmail.com>
2019-02-28 14:41:26 -0800

    set up resolvers

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,16 @@
+const Mutation = {
+	async createItem(parent, args, ctx, info) {
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					...args
+				}
+			},
+			info
+		);
+
+		return item;
+	}
+};
+
+module.exports = Mutation;
